1aac4d8fa0c6aed16a807936f5f501cb49c4d733
Tredicibrando <nicolosigiov@gmail.com>
2022-02-14 17:28:00 +0100

    pari dispari

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// console.log('ciao')
+//palindroma
 
 // Palindroma:
 // Chiedere all'utente di inserire una parola e creare 
@@ -13,31 +13,82 @@
     
 
 
-let userWord = prompt('inserisci la tua parola');
-let wordSplitted = userWord.split("");
-let palindroma = wordSplitted.reverse("");
-palindroma = palindroma.join("");
+// const userWord = prompt('inserisci la tua parola').toLowerCase();
+
+
+// function palidrom (parola){
+//     parola = userWord
+//     let wordSplitted = userWord.split("");
+//     let palindroma = wordSplitted.reverse("");
+//     palindroma = palindroma.join("");
+
+//     console.log(parola)
+//     console.log(wordSplitted)
+//     console.log (palindroma)
+
+//     if (userWord == palindroma){
+//         console.log('la parola è palindroma');
+//     } else {
+//         console.log('la parola non è palindroma');
+//     }
+
+// }
+
+// console.log(palidrom(userWord))
 
 
 
-console.log(userWord)
-console.log(wordSplitted)
-console.log (palindroma)
 
-if (userWord == palindroma){
-    console.log('la parola è palindroma');
-} else {
-    console.log('la parola non è palindroma');
+// pari e dispari
+
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+//Generiamo un numero random (sempre da 1 a 5) per il computer 
+// (usando una funzione). Sommiamo i due numeri 
+//Stabiliamo se la somma dei due numeri è pari o dispari 
+//(usando una funzione) Dichiariamo chi ha vinto.
+
+//step 1 : userInput -- numero da 1 a 5
+//step 2 : randomNumber -- genero un numero randomico tra 1 e 5
+//step 3 : sommaInput == userInput + randomNumber 
+//step 4 : dichiaro variabile pari in :false; 
+//step 4 : pongo la condizione IF ( sommaInput % 2 == 0 ) che avrà dentro la variabile =true;
+//step 5 : console.log
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
 }
 
+let userInput = parseInt(prompt('inserisci un numero da 1 a 5'));
+let randomNumber = getRandomIntInclusive(1,5);
+let sommaInput = 0;
+let sommaPari = false;
 
-// if (userWord === palindroma){
-//     console.log(userWord)
-// } else if (userWord !== palindroma){
-//     console.log()
-// } else {
-//     console.log()
-// }
+console.log(userInput, randomNumber);
+
+if (!isNaN(userInput)) {
+    sommaInput = userInput + randomNumber;
+ } 
+
+ console.log(sommaInput);
+
+if ( sommaInput % 2 == 0){
+    sommaPari = true;
+    console.log(sommaInput += ' è pari');
+} else {
+     console.log(sommaInput += ' è dispari');
+ }
+
+
+function checkpari ( userInput ){
+    
+}
+ 
+
+
+
+
 
 
 
